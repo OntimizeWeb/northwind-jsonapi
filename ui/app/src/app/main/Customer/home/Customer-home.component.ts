3df8bbbffc8b8fb242629036028f98300c54ec05
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2024-09-23 13:15:40 +0200

    Added o-tree in customers

--- ui/app/src/app/main/Customer/home/Customer-home.component.ts
+++ ui/app/src/app/main/Customer/home/Customer-home.component.ts
@@ -1,25 +1,13 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { OTableButtonComponent, OTableComponent } from 'ontimize-web-ngx';
-import { OChartModule } from 'ontimize-web-ngx-charts';
-import {OReportModule,OReportStoreService} from 'ontimize-web-ngx-report'
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'Customer-home',
   templateUrl: './Customer-home.component.html',
-  styleUrls: ['./Customer-home.component.scss']
+  styleUrls: ['./Customer-home.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class CustomerHomeComponent implements AfterViewInit {
+export class CustomerHomeComponent{
 
-  @ViewChild('table', { static: true }) table: OTableComponent;
 
-  @ViewChild('button')
-  protected button: OTableButtonComponent;
 
-  ngAfterViewInit() {
-    //this.button.onClick.subscribe(event => {
-   //   this.reportStoreService.openFillReport("94fa9d2a-e9cc-458a-a680-9bc576e14a38");
-   // });
-  }
-
-  //constructor(private reportStoreService: OReportStoreService) { }
 }
